309953dd72a9f22846f35513765f465e221eca96
ashing-zhang <2415199241@qq.com>
2026-06-09 16:55:58 +0800

    Ignore sql directory

--- sql/min_max_price.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  MIN(CAST(price AS double)) AS min_price,
-  MAX(CAST(price AS double)) AS max_price
-FROM dwd_hop_dbo_hst_order_prices_c
-WHERE pt = MAX_PT('dwd_hop_dbo_hst_order_prices_c')
-  AND hid = 2732704
